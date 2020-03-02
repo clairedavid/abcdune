2ecfd9c98976f661646c7274441d4304f7631402
clairedavid <davidc@lxplus760.cern.ch>
2020-03-02 08:35:06 +0100

    Upload all files.

---
+++ convert_glossary_tex_to_html.py
@@ -0,0 +1,235 @@
+#!/usr/local/bin/python3
+
+# ##########################################################
+# 
+# Script to convert the DUNE LaTeX glossary into HTML
+#
+# Glossary:      
+# https://github.com/DUNE/dune-tdr/blob/master/common/glossary.tex
+#
+# Author:        Claire David    cdavid@fnal.gov
+# Date created:  February 24, 2020
+# Status:        dev
+#
+#                TODO: create defHTML
+#                      1. convert \\gls{___} to HTML links
+#                      2. find LaTeX2HTML lib 
+#          
+# ##########################################################
+
+import sys
+import re
+import argparse
+import json
+import string
+
+def replace_percent_in_html_code(line):
+    
+    """
+    escaped percent replaced by HTML code
+    """
+    line = line.replace("\%", "&percnt;")
+    return(line)
+
+def remove_comment(line):
+    
+    """
+        removing all LaTeX comments (full line or end of line)
+    """
+    if "%" not in line:
+        return(line.rstrip())
+    else:
+        index_percentage = line.index("%")
+        line_with_no_comment = line[:index_percentage]
+        return(line_with_no_comment.rstrip())
+
+def latex_into_html(string, DUNEdict):
+ 
+    # Temporary: replace \\fnal and \\surf by LaTeX glossary pointers:
+    string = string.replace("\\fnal", "\\gls{fnal}")
+    string = string.replace("\\surf", "\\gls{surf}")
+    string = string.replace("\\single", "\\gsl{sp}")
+    string = string.replace("\\dual", "\\gsl{dp}")
+    string = string.replace("\\glspl{", "\\gls{") # plural won't change HTML
+
+    # List gls/glssp acronym references in LaTeX 
+    re_gls = re.compile(r'\\gls\{(.*?)\}')
+    glsTags = re_gls.findall(string)
+
+    for glsTag in glsTags:
+        tagToReplace = "\\gls{" + glsTag + "}"
+        # Get term of referenced word:
+        term = DUNEdict[glsTag]["term"]
+        string = string.replace(tagToReplace, "<a href=\"#" + glsTag + "\">" + term + "</a>")
+    
+    string = string.replace("  ", " ")
+
+    return string + "." 
+
+def main():
+
+    parser = argparse.ArgumentParser(description='Store all DUNE words and acronyms from the LaTeX glossary into a JSON file and HTML index.')
+    parser.add_argument('infile', nargs='?', type=argparse.FileType('r'), help='Name of glossary tex file.' )
+
+    args = parser.parse_args()
+    
+    if args.infile is not None:
+        filename = args.infile
+    else:
+        print("No argument given, opening default file 'glossary.tex'")
+        filename = 'glossary.tex'
+
+    with open(filename, "r") as f:
+        content = f.readlines()
+
+    # STEP 1: the escaped LaTeX percent is replaced with its HTML code
+    content_percntHTML = [ replace_percent_in_html_code(line) for line in content]
+
+    # STEP 2: now removing LaTeX comments
+    content_no_comment = [ remove_comment(line) for line in content_percntHTML if "\\newcommand" not in line]
+
+    # STEP 3: fill list of "\\new" LaTeX items: [command|duneword|duneabbrev|duneabbrevs]
+    one_line_glossary = "".join(content_no_comment)
+    one_line_glossary = one_line_glossary.replace("\n","")
+    one_line_glossary = one_line_glossary.replace("} {", "}{")
+    list_glossary = one_line_glossary.split("\\new")
+
+
+
+    # STEP 4: storing all DUNE words in dictionary into format:
+    #__________________________________________________________________
+    #
+    #                 label     | abbrev |  term  |  terms | description
+    #__________________________________________________________________
+    # LaTeX args:  
+    # newduneword      [1]      |        |   [2]  |        |   [3]   
+    # newduneabbrev    [1]      |  [2]   |   [3]  |        |   [4]
+    # newduneabbrevs   [1]      |  [2]   |   [3]  |   [4]  |   [5]
+    #__________________________________________________________________
+    # 
+    # dic = [ key, [type, abbrev, term, terms , defLaTeX, defHTML ] ]
+    #__________________________________________________________________
+    #
+    # Note: the separator for the first tags is "}{" but it is present 
+    # in custom LaTeX commands inside the description
+    # Parsing thus using partition("}{")
+
+    DUNEdict = {}
+
+    for line in list_glossary:
+
+        if line.startswith( 'duneword{' ):
+            
+            # duneword{key}{term}{ description }
+            
+            key , sep,  info       = line[9:].partition("}{")
+            term, sep, description = info.partition("}{") 
+            description            = description[:-1] if description.endswith('}') else description
+            defLaTeX               = description
+            DUNEdict[key]          = {"type": "word", "term": term, "defLaTeX": defLaTeX}
+
+        elif line.startswith( 'duneabbrev{' ):
+    
+            # duneabbrev{key}{abbrev}{term}{ description }
+
+            key , sep, info1       = line[11:].partition("}{")
+            abbrev, s, info2       = info1.partition("}{")
+            term ,  s, description = info2.partition("}{")
+            defLaTeX               = description[:-1] if description.endswith('}') else description
+            DUNEdict[key]          = {"type": "abbrev", "abbrev": abbrev, "term": term, "defLaTeX": defLaTeX}
+
+        elif line.startswith( 'duneabbrevs{' ):
+    
+            # duneabbrevs{key}{abbrev}{term}{terms}{ description }
+
+            key , sep, info1       = line[12:].partition("}{")
+            abbrev, s, info2       = info1.partition("}{")
+            term ,  s, info3       = info2.partition("}{")
+            terms,  s, description = info3.partition("}{")
+            defLaTeX               = description[:-1] if description.endswith('}') else description
+            DUNEdict[key]          = {"type": "abbrevs", "abbrev": abbrev, "term": term, "terms": terms, "defLaTeX": defLaTeX}
+    
+        else:
+            continue 
+
+    #===== Description in HTML =====
+    # Need to have loaded full dictionnary to convert
+    # the referenced acronyms into html links
+    for key , info in DUNEdict.items():
+
+        defHTML = latex_into_html(info["defLaTeX"], DUNEdict)
+        info["defHTML"] = defHTML
+
+    #===== Export in JSON file =====
+    # (could later read only read from JSON)
+    jsonfile = 'DUNE_glossary.json'
+    with open(jsonfile, 'w') as fp:
+        json.dump(DUNEdict, fp, indent=4, sort_keys=True)
+    print("DUNE words exported in JSON file " + jsonfile)
+
+    #===== Write HTML content =====
+    content = r'''<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+    <link rel="stylesheet" media="screen" type="text/css" title="Design" href="css/abcdune.css">
+  <title>A B C DUNE</title>
+</head>
+<body>
+<div id="content">
+
+<center>
+<img src="gfx/abcdune_logo.png" alt="A B C DUNE" id="header">
+<h2>Speak neutrinos</h2>
+
+<p class="alphabet_menu">
+'''
+
+    # Letters of alphabet as horizontal menu:
+    abc = list(string.ascii_uppercase)
+    abc.append("&num;")
+
+    for letter in abc:
+        content += '<a href="#' + letter + '">' + letter + '</a>\n'
+    content += r'''</p>
+</center>
+'''
+    #==== Loop over alphabet =====
+    print("Writing HTML index file...")
+
+    currentLetter = "DUMMY"
+    doHeader = True
+
+    for letter in abc:
+
+        content += '<h1 id="'+ letter +'">'+ letter +'</h1>\n<dl>\n'
+        
+        for key, info in sorted(DUNEdict.items()):
+
+            if key.startswith(letter.lower()) or (letter is "&num;" and key[0].isdigit()):
+
+                if info["type"] is not "word": # cases abbrev and abbrevs
+                    content += '  <dt id = "' + key + '">' + info["abbrev"] + '</dt>\n'
+                    content += '  <dd>' + info["term"] + '<br>'
+                else:
+                    content += '  <dt id = "' + key + '">' + info["term"] + '</dt>\n'
+                    content += '  <dd>'
+                content += info["defHTML"] + '</dd>\n'
+            # end of letter block
+            content += '</dl>\n'
+    # end of HTML:
+    content += '<br>\n</div>\n</body>\n</html>'
+
+    #===== Export in HTML index =====
+    f_html = "./index.html"
+    with open(f_html,'w') as f:
+        f.write(content)
+    print("HTML file created in:  " + f_html)
+    
+    print("\nABC DUNE says bye. ZzzZzzZ")
+
+        
+
+if __name__ == '__main__':
+    
+    main()
